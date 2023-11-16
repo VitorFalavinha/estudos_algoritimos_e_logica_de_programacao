08b7f737003d0b0ba285de87825a3f76b1ce4022
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-11-16 11:23:07 -0300

    Update teste.py

--- Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/teste.py
+++ Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/teste.py
@@ -1,1 +1,17 @@
-print ("Olá mundo!")
+idade:  int
+salario: float
+altura: float
+genero: str
+nome: str
+
+idade = 20
+salario = 5800
+altura = 1.70
+genero = "F"
+nome = 'Maria'
+
+print(f"Idade = {idade}")
+print(f"Salário = {salario:.2f}")
+print(f"Altura = {altura:.2f}")
+print(f"Gênero = {genero}")
+print(f"Nome = {nome}")
