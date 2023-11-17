f08c6ae06cc72230c370dc07c2ea8f89a0d59366
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-11-17 19:27:35 -0300

    Update circulo.py

--- Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/Estrutura sequencial/circulo.py
+++ Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/Estrutura sequencial/circulo.py
@@ -4,6 +4,6 @@ while radius <= 0:
     print("Valor inválido!")
     radius = float(input("Digite o valor do raio do círculo: "))
 
-area = 3.14 * (radius**2)
+area = 3.14159 * (radius**2)
 
 print(f"AREA = {area:.3f}")
