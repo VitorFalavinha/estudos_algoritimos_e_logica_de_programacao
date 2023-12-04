41ff3ea76a2c1145e2cf689bfadd15e76efcfea1
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-04 11:10:30 -0300

    Create rotina_soma.py

---
+++ Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/Estrutura função/rotina_soma.py
@@ -0,0 +1,12 @@
+def somaFunction():
+    soma = 0
+    while soma < 10:
+        a = int(input('Digite um numero: '))
+        b = int(input('Digite outro numero: '))
+        soma = a + b
+        print('Soma dos números digitados = ',soma)
+        print()
+
+print('OLÁ, VAMOS BRINCAR DE SOMAR? ')
+print()
+somaFunction()
