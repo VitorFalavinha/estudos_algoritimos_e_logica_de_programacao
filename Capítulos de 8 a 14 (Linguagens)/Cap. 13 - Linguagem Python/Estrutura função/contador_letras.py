4d0833ac0e276202377056ce28e6b1466b0d9e9e
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2023-12-04 11:40:41 -0300

    Create contador_letras.py

---
+++ Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/Estrutura função/contador_letras.py
@@ -0,0 +1,8 @@
+def contadorLetras(palavra):
+    tam = len(palavra)
+    print(f'A palavra {palavra} possui {tam} letras.')
+        
+
+palavra = str(input('Digite uma palavra: '))
+contadorLetras(palavra)
+
