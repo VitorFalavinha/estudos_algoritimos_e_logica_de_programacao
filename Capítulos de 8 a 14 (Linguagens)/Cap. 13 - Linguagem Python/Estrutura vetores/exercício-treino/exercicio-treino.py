0420a009204a6c4981d94ad04f691bdcc764828f
VitorFalavinha <135779845+VitorFalavinha@users.noreply.github.com>
2024-02-15 21:20:52 -0300

    Create exercicio-treino.py

---
+++ Capítulos de 8 a 14 (Linguagens)/Cap. 13 - Linguagem Python/Estrutura vetores/exercício-treino/exercicio-treino.py
@@ -0,0 +1,23 @@
+N = int(input('Numero de pessoas cadastradas: '))
+
+height:[float] = [0 for x in range(N)]
+gender:[str] = [0 for x in range(N)]
+
+somaHeight = 0
+genderMale = 0
+genderFem = 0
+for i in range (N):
+    height[i] = float(input(f'Altura da {i+1}ª pessoa: '))
+    gender[i] = str(input(f'Gênero da {i+1}ª pessoa: '))
+    somaHeight = somaHeight + height[i]
+
+    if gender[i] == 'M':
+        genderMale = genderMale + 1
+    else:
+        genderFem = genderFem + 1 
+
+mediaHeight = somaHeight/N 
+
+print(f'A média das alturas das pessoas digitadas é: {mediaHeight:.2f}')
+print(f'Foram digitados os dados de {genderFem} mulheres e {genderMale} homens.')
+    
